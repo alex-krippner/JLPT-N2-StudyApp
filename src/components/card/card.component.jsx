92b48413f5c3daba84c0a64aa547c7f92df03d1d
Alex Krippner <alexakrippner@gmail.com>
2021-01-27 11:25:40 +0100

    fix: card component rerender --> writing callback function for React.memo wrapper

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -108,7 +108,41 @@ const Card = ({
   );
 };
 
-export default React.memo(Card);
+const compareProps = function compareProps(prevProps, nextProps) {
+  const prevCardDataKeys = Object.keys(prevProps.cardData);
+  const comparePropsArray = [];
+
+  prevCardDataKeys.forEach((k) => {
+    const previousCardDataValue = prevProps.cardData[k];
+    const nextCardDataValue = nextProps.cardData[k];
+
+    // If the value of a cardData property is an array than compare each element
+    if (Array.isArray(previousCardDataValue)) {
+      const arrayDeepEquality = nextCardDataValue.every((el, idx) => {
+        if (
+          previousCardDataValue.length !== nextCardDataValue.length
+        ) {
+          return false;
+        }
+        return el === previousCardDataValue[idx];
+      });
+
+      console.log(k, nextCardDataValue, arrayDeepEquality);
+
+      comparePropsArray.push(arrayDeepEquality);
+      return;
+    }
+    comparePropsArray.push(
+      previousCardDataValue === nextCardDataValue,
+    );
+  });
+
+  return comparePropsArray.every((e) => e === true);
+};
+
+export default React.memo(Card, (prevProps, nextProps) =>
+  compareProps(prevProps, nextProps),
+);
 
 Card.propTypes = {
   cardData: PropTypes.objectOf(
@@ -122,3 +156,18 @@ Card.propTypes = {
   tabLabels: PropTypes.arrayOf(PropTypes.string).isRequired,
   label: PropTypes.string.isRequired,
 };
+
+// => {
+//   const prevCardDataKeys = Object.keys(prevProps.cardData);
+
+//   const comparePropsArray = [];
+
+//   prevCardDataKeys.forEach((k, i) => {
+//     comparePropsArray.push(
+//       prevProps.cardData[prevCardDataKeys[i]] ===
+//         nextProps.cardData[prevCardDataKeys[i]],
+//     );
+//   });
+
+//   return comparePropsArray.every((e) => e === true);
+// }
