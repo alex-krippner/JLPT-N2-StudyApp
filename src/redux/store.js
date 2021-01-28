96f7ee80185596e0642960e4050000b2f81c06ae
Alex Krippner <alexakrippner@gmail.com>
2021-01-28 12:36:48 +0100

    refactor: createStore replaced with configureStore

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,50 @@
-import { createStore, applyMiddleware } from 'redux';
-import { persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+import {
+  configureStore,
+  getDefaultMiddleware,
+} from '@reduxjs/toolkit';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [logger];
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: [
+    'kanjiCollectionReducer',
+    'vocabCollectionReducer',
+    'grammarCollectionReducer',
+    'readingCollectionReducer',
+  ],
+};
 
-export const store = createStore(
-  rootReducer,
-  applyMiddleware(...middlewares),
-);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [
+        FLUSH,
+        REHYDRATE,
+        PAUSE,
+        PERSIST,
+        PURGE,
+        REGISTER,
+      ],
+    },
+  }).concat(logger),
+});
 
 export const persistor = persistStore(store);
