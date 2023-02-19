da90a3b9e97c154790800e61bd9355262d61aa24
Alex Krippner <alexakrippner@gmail.com>
2023-02-19 12:27:30 +0100

    MK-5: Remove libraries
    The libraries directory was removed from the project's source
    directory because those have been replaced by the packages

--- src/libraries/theme/styled.d.ts
+++
@@ -1,8 +0,0 @@
-import { ColorTheme, SizeTheme } from "./Theme";
-
-declare module "styled-components" {
-  export interface DefaultTheme {
-    sizeOf: SizeTheme;
-    colorOf: ColorTheme;
-  }
-}
--- src/libraries/utilities/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./hooks";
-
-export const pipe = (...fns: any) => (initialValue: any) => {
-  return fns.reduce((currentValue: any, currentFn: any) => {
-    return currentFn(currentValue);
-  }, initialValue);
-};
