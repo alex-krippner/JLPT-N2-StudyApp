2dfc5e02b85818866878293317fd8b236c867601
Alex Krippner <alexakrippner@gmail.com>
2023-03-05 01:54:21 +0100

    MK-11: Use full screen loading component

--- src/core/context/RouterProvider.tsx
+++ src/core/context/RouterProvider.tsx
@@ -6,7 +6,7 @@ import {
 } from "react-router-dom";
 import { AuthenticationGuard } from "../components/AuthenticationGuard";
 import { AuthLayout } from "../components/AuthLayout";
-import { CallbackPage } from "../components/CallbackPage";
+import { FullScreenLoadingIndicator } from "../components/FullScreenLoadingIndicator";
 import { Home } from "../components/HomeView";
 import KanjiView from "../../features/kanji/components/KanjiView";
 
@@ -14,13 +14,13 @@ export const monRouter = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<AuthLayout />}>
       <Route index element={<Home />} />
-      <Route path="/callback" element={<CallbackPage />} />
+      <Route path="/callback" element={<FullScreenLoadingIndicator />} />
       <Route
         path="kanji"
         element={
           <AuthenticationGuard
             component={KanjiView}
-            redirectComponent={() => <div>Loading...</div>}
+            redirectComponent={() => <FullScreenLoadingIndicator />}
           />
         }
       />
