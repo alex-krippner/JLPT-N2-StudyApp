f843f79b8f254693b7f125686fd26663dceb2e07
Alex Krippner <alexakrippner@gmail.com>
2023-04-07 17:13:30 +0200

    MK-22: Fix eslint parser issue

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,13 @@
 module.exports = {
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+  ],
   parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["./tsconfig.eslint.json", "./packages/*/tsconfig.json"],
+  },
   plugins: ["@typescript-eslint"],
   root: true,
   env: {
