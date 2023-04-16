bf4c683006b40406f92da4cc49e3db46bba1586b
Alex Krippner <alexakrippner@gmail.com>
2023-04-16 23:07:53 +0200

    MK-21: Add login/out buttons

---
+++ src/core/components/LogoutButton.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Button, Typography } from "@mon/mon-ui-kit";
+
+export function LogoutButton() {
+  const { logout } = useAuth0();
+
+  const handleLogout = () => {
+    logout({
+      logoutParams: {
+        returnTo: window.location.origin,
+      },
+    });
+  };
+
+  return (
+    <Button className="button__login" onClick={handleLogout}>
+      <Typography variant="h5">Log out</Typography>
+    </Button>
+  );
+}
