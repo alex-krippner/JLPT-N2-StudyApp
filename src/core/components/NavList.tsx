a6304c0808d9a6c2f3a248bd9042ca445013d112
Alex Krippner <alexakrippner@gmail.com>
2023-04-16 23:17:54 +0200

    MK-21: Add NavList component

---
+++ src/core/components/NavList.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { List } from "@mon/mon-ui-kit";
+import { ListItemLink } from "./ListItemLink";
+import { LogoutButton } from "./LogoutButton";
+import { LoginButton } from "./LoginButton";
+
+export interface NavListItemData {
+  text: string;
+  icon: JSX.Element;
+  to: string;
+}
+
+interface NavListProps {
+  listItems: NavListItemData[];
+}
+
+export function NavList({ listItems }: NavListProps) {
+  const { isAuthenticated } = useAuth0();
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const handleListItemClick = (index: number) => {
+    setSelectedIndex(index);
+  };
+
+  return (
+    <List sx={{ display: "flex", flexDirection: "column" }}>
+      {isAuthenticated ? <LogoutButton /> : <LoginButton />}
+      {listItems.map(({ text, icon, to }, index) => (
+        <ListItemLink
+          key={text}
+          text={text}
+          icon={icon}
+          to={to}
+          index={index}
+          handleListItemClick={handleListItemClick}
+          selectedIndex={selectedIndex}
+        />
+      ))}
+    </List>
+  );
+}
