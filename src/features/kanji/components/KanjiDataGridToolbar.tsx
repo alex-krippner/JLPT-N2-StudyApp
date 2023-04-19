85fa3a4b1bf1d116ffd080ec75b8b88243c2f6a4
Alex Krippner <alexakrippner@gmail.com>
2023-04-19 21:10:36 +0200

    MK-21: Add KanjiView component

---
+++ src/features/kanji/components/KanjiDataGridToolbar.tsx
@@ -0,0 +1,52 @@
+import * as React from "react";
+import { randomUUID } from "crypto";
+import {
+  Button,
+  Typography,
+  AddIcon,
+  GridRowModes,
+  GridRowModesModel,
+  GridRowsProp,
+  GridToolbarContainer,
+} from "@mon/mon-ui-kit";
+
+interface KanjiDataGridToolbarProps {
+  setRows: (newRows: (oldRows: GridRowsProp) => GridRowsProp) => void;
+  setRowModesModel: (
+    newModel: (oldModel: GridRowModesModel) => GridRowModesModel,
+  ) => void;
+}
+export function KanjiDataGridToolbar(props: KanjiDataGridToolbarProps) {
+  const { setRows, setRowModesModel } = props;
+
+  const addRow = () => {
+    const id = randomUUID();
+    setRows((oldRows) => {
+      return [
+        ...oldRows,
+        {
+          id,
+          kanji: "",
+          kunReading: "",
+          onReading: "",
+          exampleWords: "",
+          exampleSentences: "",
+          isNew: true,
+        },
+      ];
+    });
+    setRowModesModel((oldModel) => ({
+      ...oldModel,
+      [id]: { mode: GridRowModes.Edit, fieldToFocus: "kanji" },
+    }));
+  };
+  return (
+    <GridToolbarContainer>
+      <Button color="primary" startIcon={<AddIcon />} onClick={addRow}>
+        <Typography variant="h5" color="primary.main">
+          Add kanji
+        </Typography>
+      </Button>
+    </GridToolbarContainer>
+  );
+}
--- src/features/kanji/components/KanjiView.tsx
+++ src/features/kanji/components/KanjiView.tsx
@@ -1,5 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  TableViewIcon,
+  ViewModuleIcon,
+  Box,
+  Card,
+  CardDetail,
+  IconButton,
+  Grid,
+  Typography,
+} from "@mon/mon-ui-kit";
+import { useAllKanji } from "../hooks";
+import { KanjiGrid } from "./KanjiGrid";
+import { camelCaseToWords } from "../../../utils";
 
 export function KanjiView() {
-  return <div>Kanji view</div>;
+  const { data } = useAllKanji();
+  const [isTableView, setTableView] = useState(false);
+
+  return (
+    <Box sx={{ display: "flex", height: "100%", flexDirection: "column" }}>
+      <Box>
+        <IconButton
+          sx={{ mb: 4 }}
+          onClick={() => setTableView(!isTableView)}
+          size="large"
+        >
+          {isTableView ? (
+            <ViewModuleIcon color="primary" fontSize="medium" />
+          ) : (
+            <TableViewIcon color="primary" fontSize="medium" />
+          )}
+        </IconButton>
+      </Box>
+      {isTableView ? (
+        <KanjiGrid data={data} />
+      ) : (
+        <Grid container spacing={5} overflow="auto">
+          {data ? (
+            data.map((k) => {
+              const { meanings, kanji, exampleWords, exampleSentences, id } = k;
+              const cardDetails: CardDetail[] = Object.entries({
+                meanings,
+                kanji,
+                exampleWords,
+                exampleSentences,
+              }).map(([header, dataText]) => ({
+                header: camelCaseToWords(header),
+                dataText,
+              }));
+
+              return (
+                <Grid item key={`VocabGridItem_${id}`}>
+                  <Card
+                    mainContent={kanji}
+                    data={cardDetails}
+                    id={id}
+                    key={id}
+                  />
+                </Grid>
+              );
+            })
+          ) : (
+            <Typography variant="h5">
+              Looks like there is no data {"\uD83D\uDE22"}
+            </Typography>
+          )}
+        </Grid>
+      )}
+    </Box>
+  );
 }
